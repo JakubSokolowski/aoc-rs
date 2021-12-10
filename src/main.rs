fd700ab73cddb706541392be8f1a5578092c7f70
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-12-10 18:26:57 +0100

    Add day10 solution

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,7 @@ fn main() {
         (2021, 7) => aoc_2021::day07::run(&common::data::read_to_string(year, day, bigboy)),
         (2021, 8) => aoc_2021::day08::run(&common::data::read_to_vec(year, day, bigboy)),
         (2021, 9) => aoc_2021::day09::run(&common::data::read_to_vec(year, day, bigboy)),
+        (2021, 10) => aoc_2021::day10::run(&common::data::read_to_vec(year, day, bigboy)),
 
         (_, _) => panic!("Not implemented"),
     }
